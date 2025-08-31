fc5d3a609a1fb445d6522f472bf9012c6eb65415
MuhammadMoiz-dev <mmoiz2162@gmail.com>
2025-08-31 16:23:54 +0500

    update file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Login from './components/Login'
+import About from './components/About'
 import './App.css'
 
 function App() {
-
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "/Login",
+      element: <Login />
+    },
+    {
+      path: "/About",
+      element: <About />
+    }
+  ])
   return (
     <>
       <Navbar />
+      <RouterProvider router={router} />
+
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-
+import { Link } from "react-router-dom"
 function Navbar() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <h1>Navbar</h1>
+            <nav>
+            <Link to='/Home'><li>home</li></Link>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar
